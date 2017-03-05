b4eddda53f6bfa870ee534e86c53b01381ffe9f1
Erico Silva <augusto.ericosilva@gmail.com>
2017-03-05 14:20:59 -0300

    Update boilerplate with test

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var Main = require('Main');
-
 // load foundation
 require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
 // app css
@@ -12,9 +10,6 @@ require('style-loader!css-loader!sass-loader!applicationStyles');
 $(document).foundation();
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-      <Route path="/" component={Main}>
-      </Route>
-  </Router>,
+  <p>React + Foundation boilerplate</p>,
   document.getElementById('app')
 );
